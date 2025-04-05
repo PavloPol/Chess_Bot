8f4a67f318aa6e9b5aae0f05c773e1eeb9948b9a
pavlo.polosenko <pavlo.polosenko@avasure.com>
2025-04-05 12:18:33 +0300

    Everything working

---
+++ AI/Matrix.cs
@@ -0,0 +1,169 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace ChessBot.AI
+//{
+//    public class Matrix
+//    {
+//        public double[,] Data { get; private set; }
+//        public int Rows { get; private set; }
+//        public int Columns { get; private set; }
+
+//        public Matrix(int rows, int columns)
+//        {
+//            Rows = rows;
+//            Columns = columns;
+//            Data = new double[rows, columns];
+//        }
+
+//        public void SetMatrix(Matrix matrix)
+//        {
+//            Rows = matrix.Rows;
+//            Columns = matrix.Columns; 
+//            Data = matrix.Data;
+//        }
+
+//        public void Randomize()
+//        {
+//            Random rand = new Random();
+//            for (int i = 0; i < Rows; i++)
+//            {
+//                for (int j = 0; j < Columns; j++)
+//                {
+//                    Data[i, j] = rand.NextDouble() * 2 - 1;
+//                }
+//            }
+//        }
+
+//        public void Add(Matrix other)
+//        {
+//            if (Rows != other.Rows || Columns != other.Columns)
+//                throw new Exception("Matrix dimensions must match for addition!");
+
+//            for (int i = 0; i < Rows; i++)
+//            {
+//                for (int j = 0; j < Columns; j++)
+//                {
+//                    Data[i, j] += other.Data[i, j];
+//                }
+//            }
+//        }
+
+//        public static Matrix Substract(Matrix a, Matrix b)
+//        {
+//            if (a.Rows != b.Rows || a.Columns != b.Columns) throw new Exeption("Matrix dimensions must match for substract!");
+
+//            Matrix result = new Matrix(a.Rows, a.Columns);
+
+//            for(int i = 0; i < a.Rows; i++)
+//            {
+//                for (int j = 0; j < a.Columns; j++)
+//                {
+//                    result.Data[i, j] = a.Data[i, j] - b.Data[i, j];
+//                }
+//            }
+
+//            return result;
+//        }
+
+//        public void Multiply(Matrix matrix)
+//        {
+//            if (Columns != matrix.Columns) throw new Exception("Incompatible matrix dimensions for multiplucations!");
+
+//            SetMatrix(Multiply(this, matrix));
+//        }
+
+//        public static Matrix Multiply(Matrix a, Matrix b)
+//        {
+//            if (a.Columns != b.Columns) throw new Exception("Incompatible matrix dimensions for multiplications!");
+
+//            Matrix result = new Matrix(a.Rows, b.Columns);
+//            for (int i = 0; i < a.Rows; i++)
+//            {
+//                for (int j = 0; j < b.Columns; j++)
+//                {
+//                    for (int k = 0; k < a.Columns; k++)
+//                    {
+//                        result.Data[i, j] += a.Data[i, k] * b.Data[k, j];
+//                    }
+//                }
+//            }
+
+//            return result;
+//        }
+
+//        public void Multiply(double multiplier)
+//        {
+//            for(int i  = 0; i < Rows; i++)
+//            {
+//                for(int j = 0; j < Columns; j++)
+//                {
+//                    Data[i, j] *= multiplier;
+//                }
+//            }
+//        }
+
+//        public void ApplyFunction(Func<double, double> function)
+//        {
+//            for (int i = 0; i < Rows; i++)
+//            {
+//                for (int j = 0; j < Columns; j++)
+//                {
+//                    Data[i, j] = function(Data[i, j]);
+//                }
+//            }
+//        }
+
+//        public static Matrix ApplyFunction(Matrix matrix, Func<double, double> function)
+//        {
+//            for(int i = 0; i < matrix.Rows; i++)
+//            {
+//                for(int j = 0; j < matrix.Columns; j++)
+//                {
+//                    matrix.Data[i, j] = function(matrix.Data[i, j]);
+//                }
+//            }
+//            return matrix;
+//        }
+
+//        public static Matrix Transpose(Matrix matrix)
+//        {
+//            Matrix result = new Matrix(matrix.Columns, matrix.Rows);
+
+//            for(int j = 0; j < matrix.Columns; j++)
+//            {
+//                for(int i = 0; i < matrix.Rows; i++)
+//                {
+//                    result.Data[j, i] = matrix.Data[i, j];
+//                }
+//            }
+
+//            return result;
+//        }
+
+//        public static Matrix ConvertArrayToMatrix(double[] data)
+//        {
+//            Matrix result = new Matrix(data.Length, 1);
+
+//            for(int i =  0; i < data.Length; i++)
+//            {
+//                result.Data[i, 0] = data[i];
+//            }
+
+//            return result;
+//        }
+
+//        public static double[] ConvertMatrixToArray(Matrix matrix)
+//        {
+//            double[] array = new double[matrix.Rows];
+//            for(int i = 0; i < matrix.Rows; i++)
+//            {
+//                array[i] = matrix.Data[i, 0];
+//            }
+//            return array;
+//        }
+//    }
+//}
---
+++ AI/NeuralNetwork.cs
@@ -0,0 +1,103 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace ChessBot.AI
+//{
+//    public class NeuralNetwork
+//    {
+//        private int inputNodes;
+//        private int hiddenNodes;
+//        private int outputNodes;
+
+//        private Matrix weightsInputHidden;
+//        private Matrix weightsHiddenOutput;
+
+//        private Matrix biasHidden;
+//        private Matrix biasOutput;
+
+//        private double learningRate = 0.1;
+
+//        public NeuralNetwork(int inputNodes, int hiddenNodes, int outputNodes)
+//        {
+//            this.inputNodes = inputNodes;
+//            this.hiddenNodes = hiddenNodes;
+//            this.outputNodes = outputNodes;
+
+//            weightsInputHidden = new Matrix(hiddenNodes, inputNodes);
+//            weightsHiddenOutput = new Matrix(outputNodes, hiddenNodes);
+//            biasHidden = new Matrix(hiddenNodes, 1);
+//            biasOutput = new Matrix(outputNodes, 1);
+
+//            weightsInputHidden.Randomize();
+//            weightsHiddenOutput.Randomize();
+//            biasHidden.Randomize();
+//            biasOutput.Randomize();
+//        }
+        
+//        //forward propagation
+//        public double[] Predict(double[] inputs)
+//        {
+//            Matrix inputMatrix = Matrix.ConvertArrayToMatrix(inputs);
+
+//            // Input to Hidden
+//            Matrix hidden = Matrix.Multiply(weightsInputHidden, inputMatrix);
+//            hidden.Add(biasHidden);
+//            hidden.ApplyFunction(ActivationFunctions.Sigmoid);
+
+//            // Hiiden to Output
+//            Matrix output = Matrix.Multiply(weightsHiddenOutput, hidden);
+//            output.Add(biasOutput);
+//            output.ApplyFunction(ActivationFunctions.Sigmoid);
+
+//            return Matrix.ConvertMatrixToArray(output);
+//        }
+
+//        // Training woth backpropagation
+//        public void Train(double[] inputsArray, double[] targetArray)
+//        {
+//            Matrix inputs = Matrix.ConvertArrayToMatrix(inputsArray);
+//            Matrix targets = Matrix.ConvertArrayToMatrix(targetArray);
+
+//            //Forward Propagation
+//            Matrix hidden = Matrix.Multiply(weightsInputHidden, inputs);
+//            hidden.Add(biasHidden);
+//            hidden.ApplyFunction(ActivationFunctions.Sigmoid);
+
+//            Matrix outputs = Matrix.Multiply(weightsHiddenOutput, hidden);
+//            outputs.Add(biasOutput);
+//            outputs.ApplyFunction(ActivationFunctions.Sigmoid);
+
+//            //Calculate output error
+//            Matrix outputErrors = Matrix.Substract(targets, outputs);
+
+//            //Backpropagation for hidden to output weights
+//            Matrix outputGradients = Matrix.ApplyFunction(outputs, ActivationFunctions.SigmoidDerivative);
+//            outputGradients.Multiply(outputErrors);
+//            outputGradients.Multiply(learningRate);
+
+//            Matrix hiddenTransposed = Matrix.Transpose(hidden);
+//            Matrix weightsHioddenOutputDeltas = Matrix.Multiply(outputGradients, hiddenTransposed);
+
+//            weightsHiddenOutput.Add(weightsHioddenOutputDeltas);
+//            biasOutput.Add(outputGradients);
+
+//            //Calculate hidden layer errors
+//            Matrix weightsHiddenOutputTransposed = Matrix.Transpose(weightsHiddenOutput);
+//            Matrix hiddenErrors = Matrix.Multiply(weightsHiddenOutputTransposed, outputErrors);
+
+//            // Backpropagation for input to hiddden weights
+//            Matrix hiddenGradients = Matrix.ApplyFunction(hidden, ActivationFunctions.SigmoidDerivative);
+//            hiddenGradients.Multiply(hiddenErrors);
+//            hiddenGradients.Multiply(learningRate);
+
+//            Matrix inputsTransposed = Matrix.Transpose(inputs);
+//            Matrix weightsInputHiddenDeltas = Matrix.Multiply(hiddenGradients, inputsTransposed);
+
+//            weightsInputHidden.Add(weightsInputHiddenDeltas);
+//            biasHidden.Add(hiddenGradients);
+//        }
+//    }
+//}
